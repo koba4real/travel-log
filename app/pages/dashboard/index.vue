8a3914f8f334984b22a34064d04220fba5547652
koba <132523021+koba4real@users.noreply.github.com>
2026-06-16 10:29:07 +0200

    SSR Auth Session Setup (#58)
    #20: not very sure it works as should, can't wrap my head around the problem , so i just copied the tuto solution and skipped it

--- app/pages/dashboard/index.vue
+++ app/pages/dashboard/index.vue
@@ -1,9 +1,3 @@
-<script setup lang="ts">
-definePageMeta({
-  middleware: "auth",
-});
-</script>
-
 <template>
   <h1>welcome to the travel log dashboard </h1>
 </template>
---
+++ server/middleware/auth.ts
@@ -0,0 +1,10 @@
+import { auth } from "~~/lib/auth";
+
+export default defineEventHandler(async (event) => {
+  if (event.path.startsWith("/dashboard")) {
+    const session = await auth.api.getSession({ headers: event.headers });
+    if (!session?.user) {
+      return sendRedirect(event, "/", 302);
+    }
+  }
+});
